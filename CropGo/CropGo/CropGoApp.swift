a06ec9a750c41cb93b9158c451f779876a7e507b
imanolsoftpital <imanol.gonzalez@softpital.com>
2024-10-22 16:12:48 -0600

    added xcode project

---
+++ CropGo/CropGo/CropGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropGoApp.swift
+//  CropGo
+//
+//  Created by Imanol Armando González Solís on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CropGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CropGo/CropGoTests/CropGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CropGoTests.swift
+//  CropGoTests
+//
+//  Created by Imanol Armando González Solís on 22/10/24.
+//
+
+import Testing
+@testable import CropGo
+
+struct CropGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
