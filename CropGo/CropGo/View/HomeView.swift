8ebd8c17b0cb0e0f26b7146821e5222a0198d4c4
imanolgzz <imanolgzzsolis@gmail.com>
2024-10-23 15:39:00 -0600

    small changes

--- CropGo/CropGo/View/HomeView.swift
+++ CropGo/CropGo/View/HomeView.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack{
-            Text("Home")
+        ScrollView(.vertical){
+            VStack{
+                Text("My Crops")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(10)
+                ScrollView(.horizontal){
+                    HStack(spacing: 25){
+                        cropHeatmap(text: "Wheat")
+                        cropHeatmap(text: "Corn", angle: 90)
+                        cropHeatmap(text: "Potatoes", angle: 180)
+                        cropHeatmap(text: "Beans", angle: 270)
+                    }
+                }
+                .padding(23)
+                HStack{Spacer()}
+            }
+
+        }.padding(0)
+            .background(Color(red:230/255, green:230/255, blue:230/255))
+    }
+}
+
+struct cropHeatmap: View {
+    var text: String
+    var angle: CGFloat = 0
+    var body: some View {
+        VStack(spacing: 10){
+            Image("Heatmap")
+                .resizable()
+                .frame(width: 120, height: 120)
+                .cornerRadius(15)
+                .rotationEffect(.degrees(angle))
+            
+            Text(text)
+                .fontWeight(.bold)
         }
+        .padding(.bottom, 20)
     }
 }
 
